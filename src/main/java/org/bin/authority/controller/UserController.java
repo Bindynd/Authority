400e4e6ad53266d4fe3eaa1d6fb73a780d5a4cef
JulyByWen <2296019069@qq.com>
2023-12-08 01:21:27 +0200

    feat:接口代码测试

--- src/main/java/org/bin/authority/controller/UserController.java
+++ src/main/java/org/bin/authority/controller/UserController.java
@@ -1,6 +1,10 @@
 package org.bin.authority.controller;
 
 
+import org.bin.authority.service.UserService;
+import org.bin.authority.utils.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
+    @Autowired
+    private UserService userService;
+    /**
+     * 查询所有用户列表
+     * @return
+     */
+    @GetMapping("/listAll")
+    public Result list(){
+        return Result.ok(userService.list());
+    }
 
 }
 
