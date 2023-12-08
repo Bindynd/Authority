23be4626a7c0db5a48723a71aea4b1da1f4338cf
JulyByWen <2296019069@qq.com>
2023-12-08 02:23:59 +0200

    feat:认证成功返回Token

--- src/main/java/org/bin/authority/config/security/handler/LoginSuccessHandler.java
+++ src/main/java/org/bin/authority/config/security/handler/LoginSuccessHandler.java
@@ -2,11 +2,16 @@ package org.bin.authority.config.security.handler;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
+import io.jsonwebtoken.Jwts;
 import org.bin.authority.entity.User;
+import org.bin.authority.utils.JwtUtils;
+import org.bin.authority.utils.LoginResult;
+import org.bin.authority.utils.ResultCode;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
@@ -19,14 +24,25 @@ import java.nio.charset.StandardCharsets;
  */
 @Component
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
+    @Resource
+    private JwtUtils jwtUtils;
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         //设置客户端的响应的内容类型
         response.setContentType("application/json;charset=UTF-8");
         //获取当登录用户信息
         User user = (User) authentication.getPrincipal();
+        //生成token
+        String token = jwtUtils.generateToken(user);
+        //设置token签名密钥及过期时间
+        long expireTime = Jwts.parser() //获取DefaultJwtParser对象
+                .setSigningKey(jwtUtils.getSecret()) //设置签名的密钥
+                .parseClaimsJws(token.replace("jwt_", ""))
+                .getBody().getExpiration().getTime();//获取token过期时间
+        //创建登录结果对象
+        LoginResult loginResult = new LoginResult(user.getId(), ResultCode.SUCCESS,token,expireTime);
         //消除循环引用
-        String result = JSON.toJSONString(user, SerializerFeature.DisableCircularReferenceDetect);
+        String result = JSON.toJSONString(loginResult, SerializerFeature.DisableCircularReferenceDetect);
         //获取输出流
         ServletOutputStream outputStream = response.getOutputStream();
         outputStream.write(result.getBytes(StandardCharsets.UTF_8));
